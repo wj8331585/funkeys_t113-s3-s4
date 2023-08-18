c775d6069c08e12b84c820fcef4bc66825ec18df
DrUm78 <cruau.david@gmail.com>
2023-08-18 11:41:07 +0200

    Updated ST-SDL package

--- FunKey/package/st-sdl/st-sdl.mk
+++ FunKey/package/st-sdl/st-sdl.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-ST_SDL_VERSION = 2c29ceeb9ec01154d5a80d394446b42b0154d0ec
+ST_SDL_VERSION = v1.0-funkey-s
 ST_SDL_SITE_METHOD = git
-ST_SDL_SITE = https://github.com/FunKey-Project/st-sdl.git
+ST_SDL_SITE = https://github.com/DrUm78/st-sdl.git
 ST_SDL_LICENSE = GPL-2.1+
 ST_SDL_LICENSE_FILES = LICENSE
 
@@ -14,7 +14,7 @@ ST_SDL_DEPENDENCIES = sdl
 
 define ST_SDL_BUILD_CMDS
 	(cd $(@D); \
-	sed -i -e 's|/opt/FunKey-sdk-2.0.0|../../host|g' config_funkey-s.mk; \
+	sed -i -e 's|/opt/FunKey-sdk|../../host|g' config_funkey-s.mk; \
 	sed -i -e 's|arm-funkey-linux-musleabihf-|arm-linux-|g' config_funkey-s.mk; \
 	make \
 	)
